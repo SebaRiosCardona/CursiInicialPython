7401b44662e59d202b4d1c4faf0355fe6aac1b77
jsebariosc <sebastian@rudol.ai>
2025-06-26 16:28:20 -0300

    clase2

---
+++ ejemploCondicionales.py
@@ -0,0 +1,9 @@
+edad = 24
+sexo = "m"
+
+if edad < 18:
+  print("no es mayor")
+elif edad >= 18 and edad <= 35:
+  print("sos un pendejo")
+else:
+  print("sos grande")
---
+++ suma.py
@@ -0,0 +1,11 @@
+def suma (a, b):
+  operacionSuma = a + b
+  return operacionSuma
+  
+resultadoSuma = suma(1, 4)
+
+def multi (a, b):
+  c = a * b
+  print(c)
+
+multi(resultadoSuma, 3)
